fd9ea4a1b2e71d89bd48f8ac2a393fadf1a31747
Cam <cameron-04@hotmail.com>
2025-01-13 13:07:43 -0600

    log chat embed urls

--- src/live_chats.js
+++ src/live_chats.js
@@ -5,15 +5,23 @@ export function LiveChats() {
     const twitchChannelCode = parseTwitchChannelCode();
     const ytVideoId = parseYTVideoId();
 
+    const ytEmbedUrl = `https://www.youtube.com/live_chat?v=${ytVideoId}&embed_domain=${window.location.hostname}`;
+    const twitchEmbedUrl = `https://www.twitch.tv/embed/${twitchChannelCode}/chat?parent=${window.location.hostname}`;
+
+    console.log({
+        ytEmbedUrl,
+        twitchEmbedUrl
+    });
+
     return <div style={{ display: "flex", minHeight: "95vh" }}>
         <iframe
             title="Youtube Chat Window"
-            src={`https://www.youtube.com/live_chat?v=${ytVideoId}&embed_domain=${window.location.hostname}`}
+            src={ytEmbedUrl}
             style={{ flex: 1, padding: "5px" }}
         ></iframe>
         <iframe
             title="Twitch Chat Window"
-            src={`https://www.twitch.tv/embed/${twitchChannelCode}/chat?parent=${window.location.hostname}`}
+            src={twitchEmbedUrl}
             style={{ flex: 1, padding: "5px" }}
         >
         </iframe>
